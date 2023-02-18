cca7d960b970c45a2425ab4df34a77c664e4b9bb
Suriyaa <suriyaaap13@gmail.com>
2023-02-18 15:09:21 +0530

    Set up a minimalistic Accordion using React

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,22 @@
-import AccordionShow from "./AccordionShow";
+import { func } from "prop-types";
+import { useState } from "react";
+
 
 export default function Accordion({ items }) {
 
-  const renderedItems = items.map(( item )=>{
-    return <AccordionShow key={item.id} item={item} />
+  const [expandedItem, setExpandedItem] = useState(0);
+
+
+  const renderedItems = items.map(( item, index )=>{
+
+    const isExpanded = index === expandedItem;
+    
+    return (
+      <div key={item.id}>
+          <div onClick={()=>setExpandedItem(index)}>{item.label}</div>
+          <div>{isExpanded&&<div>{item.content}</div>}</div>
+      </div>
+    )
   });
 
   return (
