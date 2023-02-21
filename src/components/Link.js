79c7382923c3ccf139a0a58790c34ebd90e5e319
Suriyaa <suriyaaap13@gmail.com>
2023-02-21 14:26:00 +0530

    Create custom Hook for navigation

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import useNavigation from '../hooks/use-navigation'
 
 export default function Link({to, children}) {
 
-    const { navigate } = useContext(NavigationContext);
+    const { navigate } = useNavigation();
 
     function handleClick(event){
+        if(event.metaKey || event.ctrlKey)
+            return;
         event.preventDefault();
         navigate(to);
     }
 
     return (
-        <a href={to} onClick={handleClick}>
+        <a className='text-blue-500' href={to} onClick={handleClick}>
             {children}
         </a>
     )
