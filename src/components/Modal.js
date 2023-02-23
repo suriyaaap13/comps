201e88c7b4f40310d1404853e701faec460416b1
Suriyaa <suriyaaap13@gmail.com>
2023-02-24 00:49:52 +0530

    Complete modal with appropriate features

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,22 @@
 import ReactDOM from "react-dom";
 
-export default function Modal({onClose}) {
+export default function Modal({onClose, children, actionButton}) {
+
+
+
     return ReactDOM.createPortal(
-        <>
-            <div className="absolute inset-0 bg-gray-300 opacity-80" onClick={onClose}></div>
-            <div className="absolute inset-40 bg-white flex justify-center items-center">I'm a modal</div>
-        </>,
+        <div>
+            <div className="fixed inset-0 bg-gray-300 opacity-80" onClick={onClose}></div>
+            <div className="fixed inset-40 bg-white flex justify-center items-center p-5 h-40">
+                <div className="flex flex-col justify-between">
+                    {children}
+                    <div className="flex justify-end mt-7">
+                        {actionButton}
+                    </div>
+                </div>
+            </div>
+            
+        </div>,
         document.querySelector('.modal-container')
     );
 }
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -14,10 +14,37 @@ export default function ModalPage() {
     setShowModal(false);
   }
 
+  const actionButton = <Button primary onClick={handleClose}>I Accept</Button>
+
+  const modal = <div>
+    <Modal 
+      onClose = {handleClose} 
+      actionButton = {actionButton}
+      className = ""
+    >
+      I here have read all the important information and have no problem in them
+    </Modal>
+    
+  </div>;
+
+  document.body.classList.add('overflow-hidden');
+
+  
+
   return (
     <div>
       <Button primary rounded onClick = {handleClick}>Open Modal</Button>
-      {showModal && <div><Modal onClose = {handleClose} /></div>}
+      {showModal && modal}
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula finibus tellus nec interdum. Sed commodo lorem leo. Nullam eget purus elit. Nullam vitae felis a ligula gravida rhoncus id vel risus. Nam rutrum sollicitudin pellentesque. Integer lacinia suscipit turpis nec scelerisque. Pellentesque eleifend bibendum lorem nec posuere. Ut ornare augue velit, egestas fringilla nisi pretium ut.
+
+      Ut quis dolor volutpat, tincidunt metus ut, mollis nibh. Nulla sit amet justo sem. Nam semper placerat est sed luctus. Nulla viverra, tortor ut rutrum varius, nibh turpis ultricies tellus, eu sodales ipsum nunc vitae lectus. Mauris vel finibus est, a mollis lacus. Quisque et dui leo. Donec non condimentum urna. Curabitur consequat neque in nulla fringilla, et posuere leo rhoncus. Quisque venenatis mollis cursus. Phasellus erat justo, efficitur maximus placerat vel, auctor ac diam. Cras lectus diam, tempus ut vestibulum a, auctor a nisi.
+
+      Nulla in euismod diam, quis sollicitudin risus. Curabitur metus nisl, consectetur nec purus quis, rhoncus ultrices tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sit amet tortor volutpat, ultrices ipsum non, porttitor leo. Suspendisse magna augue, scelerisque tincidunt mauris quis, fringilla iaculis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi id justo vestibulum, interdum sapien sed, iaculis purus. Vivamus interdum, odio ultricies bibendum bibendum, nibh tellus hendrerit nisl, euismod aliquet metus magna nec neque. Vivamus cursus ac orci at porttitor. Donec vitae lobortis tellus, non aliquet nulla. Maecenas imperdiet lectus nec lectus feugiat suscipit.
+
+      Nulla malesuada diam vel molestie semper. Integer condimentum arcu eu velit commodo posuere. Donec eu facilisis enim. Praesent eget elit sagittis, ullamcorper tortor id, iaculis nisi. Proin dui mauris, sagittis eu ligula non, aliquet mollis urna. Sed elementum dolor non ex blandit imperdiet. Donec quis felis purus.
+
+      Ut ornare imperdiet sapien, ut mattis dolor tempus at. Aliquam eros metus, pulvinar quis tempus malesuada, cursus at tellus. Fusce sed erat nec arcu placerat tincidunt. Sed eros mauris, accumsan a magna ut, consectetur commodo odio. Cras maximus vitae sapien in dignissim. Ut volutpat pellentesque libero ut tempor. Praesent sed facilisis odio, eu finibus purus. Aliquam feugiat neque et turpis cursus, et efficitur turpis porttitor. Donec tellus erat, imperdiet non varius at, vestibulum sit amet libero. Maecenas interdum est est, non fringilla lacus rutrum et. Proin cursus, ex vel pellentesque dignissim, sapien erat mattis urna, eu maximus urna purus ac urna.
     </div>
-  )
+  );
+
 }
