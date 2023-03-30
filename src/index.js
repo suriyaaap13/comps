119a1039395c48e36e476fcf54c6427a361780e5
Suriyaa <suriyaaap13@gmail.com>
2023-03-30 10:46:33 +0530

    Learning redux tool kit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import store from './app/store';
+import { Provider } from 'react-redux';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-root.render(<App/>)
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+
+)
